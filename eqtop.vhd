02ced142be91966612fc0b3f4aa7ec8dfdf08c14
Luca <91071096+LucaAvila123@users.noreply.github.com>
2022-06-08 14:27:31 -0300

    Update eqtop.vhd

--- eqtop.vhd
+++ eqtop.vhd
@@ -1,38 +1,8 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 06/03/2022 02:10:05 PM
--- Design Name: 
--- Module Name: eqtop - arch
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
 use IEEE.NUMERIC_STD.ALL;
 
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
 entity eqtop is
---  Port ( );
     port(
         
           sw    : in  std_logic_vector(7 downto 0);
@@ -48,10 +18,8 @@ begin
         port map(
             shift => sw(7 downto 5),
             x     => sw(4 downto 0),
-            
-            saida => led(7 downto 0)
-            
-            
+            saida => led(7 downto 0) 
+           
         );
 
 end arch;
